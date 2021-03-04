5074dda69a86db77a8d4422b0979ad8177060882
lucaperullo <72197904+lucaperullo@users.noreply.github.com>
2021-03-04 17:43:06 +0100

    up

--- fetch.js
+++ fetch.js
@@ -1,9 +1,74 @@
+const likedSongs = [];
+
+async function getAlbums() {
+  let search = document.querySelector(".spoti-search").value;
+  const response = await fetch(
+    "https://spotify-fetch.herokuapp.com/https://api.deezer.com/search?q=" +
+      search
+    // "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + search, //query
+    // {
+    //   headers: {
+    //     "x-rapidapi-key": "747030d749msh471c14b186c043cp1cfee9jsn05041c6abfa1",
+    //     "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    //   },
+    // }
+  );
+  const data = await response.json();
+  console.log(data);
+  displayResults(data.data);
+  return data;
+}
+const displayResults = (data) => {
+  let div = document.querySelector("#searchResults");
+
+  div.innerHTML = "";
+  data.forEach((element) => {
+    let text = document.createElement("div");
+    let texty = document.createElement("p");
+    let card = document.createElement("div");
+    let img = document.createElement("img");
+    let cardContainer = document.createElement("div");
+    let containerCardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container-spotify");
+    containerCardContainer.classList.add("col-8");
+    img.classList.add("card-image");
+    card.classList.add("card-body");
+    texty.classList.add("card-text");
+    texty.innerText = `${element.album.title}`;
+    text.classList.add("info");
+    img.setAttribute("src", element.album.cover);
+    img.classList.add("search-result");
+
+    card.appendChild(img);
+
+    text.appendChild(texty);
+    card.appendChild(text);
+    cardContainer.appendChild(card);
+    containerCardContainer.appendChild(cardContainer);
+    div.appendChild(containerCardContainer);
+  });
+};
+const submitSearch = () => {
+  const input = document.querySelector(".spoti-search");
+  console.log(input);
+  input.addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+      console.log("hi");
+
+      getAlbums();
+    }
+  });
+};
+
+///////////////////////////////////////////////////////////////
+///////////////////// ALBUM CATEGORIES ////////////////////////
+///////////////////////////////////////////////////////////////
+
 const trendingNowAlbums = [
   "180681412",
   "180996332",
   "178086012",
   "180983992",
-  "181446752",
   "181270962",
   "179934622",
   "180450492",
@@ -156,112 +221,61 @@ const playlistsJustForYou = [
   "1964085082",
   "2098157264",
 ];
-const likedSongs = [];
 
-async function getAlbums() {
-  let search = document.querySelector(".spoti-search").value;
-  const response = await fetch(
-    "https://spotify-fetch.herokuapp.com/https://api.deezer.com/search?q=" +
-      search
-    // "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + search, //query
-    // {
-    //   headers: {
-    //     "x-rapidapi-key": "747030d749msh471c14b186c043cp1cfee9jsn05041c6abfa1",
-    //     "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-    //   },
-    // }
-  );
-  const data = await response.json();
-  console.log(data);
-  displayResults(data.data);
-  return data;
-}
-const displayResults = (data) => {
-  let div = document.querySelector("#searchResults");
+///////////////////////////////////////////////////////////////
+///////////////////////// ALBUMS FETCHES///////////////////////
+///////////////////////////////////////////////////////////////
 
-  div.innerHTML = "";
-  data.forEach((element) => {
-    let text = document.createElement("div");
-    let texty = document.createElement("p");
-    let card = document.createElement("div");
-    let img = document.createElement("img");
-    let cardContainer = document.createElement("div");
-    let containerCardContainer = document.createElement("div");
-    cardContainer.classList.add("card-container-spotify");
-    containerCardContainer.classList.add("col-8");
-    img.classList.add("card-image");
-    card.classList.add("card-body");
-    texty.classList.add("card-text");
-    texty.innerText = `${element.album.title}`;
-    text.classList.add("info");
-    img.setAttribute("src", element.album.cover);
-    img.classList.add("search-result");
-
-    card.appendChild(img);
-
-    text.appendChild(texty);
-    card.appendChild(text);
-    cardContainer.appendChild(card);
-    containerCardContainer.appendChild(cardContainer);
-    div.appendChild(containerCardContainer);
-  });
-};
-const submitSearch = () => {
-  const input = document.querySelector(".spoti-search");
-  console.log(input);
-  input.addEventListener("keydown", function (event) {
-    if (event.keyCode === 13) {
-      console.log("hi");
-
-      getAlbums();
-    }
-  });
-};
+let albumURL =
+  "https://spotify-fetch.herokuapp.com/https://api.deezer.com/album/"; //*album id
+let artistURL =
+  "https://spotify-fetch.herokuapp.com/https://api.deezer.com/artist/"; //+artist id
+let chartURL = "https://spotify-fetch.herokuapp.com/https://api.deezer.com/"; //+chart
+let genreURL =
+  "https://spotify-fetch.herokuapp.com/https://api.deezer.com/genre/0"; //+ genre number
+let optionsURL =
+  "https://spotify-fetch.herokuapp.com/https://api.deezer.com/options/";
+// + options (streaming	If the user can stream on the platform  :	boolean
+// streaming_duration:	the streaming duration of the user  :	int
+// offline:	The user can listen to the music in offline mode  :	boolean
+// hq:	The HQ can be activated  :	boolean
+// ads_display:	Displays ads  :	boolean
+// ads_audio:	Activates audio ads  :	boolean
+// too_many_devices:	If the user reached the limit of linked devices  :	boolean
+// can_subscribe:	If the user can subscribe to the service:	boolean
+// radio_skips:	The limit of radio skips. 0 = no limit  :	int
+// lossless:	Lossless is available	:  boolean
+// preview:	Allows to display the preview of the tracks  :	boolean
+// radio:	Allows to stream the radio	:  boolean)
+let playlistURL =
+  "https://spotify-fetch.herokuapp.com/https://api.deezer.com/playlist/";
+//+ id(int), title(string), description(string), duration(int),is_loved_track(boolean),
+//nb_tracks(int), fans(The number of playlist's fans(int))
 
 const showData = () => {
-  const albums = [
-    "6966955",
-    "61419582",
-    "7090505",
-    "103248",
-    "119606",
-    "212377",
-  ];
-  albums.forEach((id) => {
-    showAlbums(id);
+  trendingNowAlbums.forEach(async (id) => {
+    await showAlbums(id);
     async function showAlbums(id) {
-      let firstRow = document.getElementById("good-time-row");
-
-      let column = document
-        .createElement("div")
-        .classList.add("col-sm-12", "col-md-6", "col-lg-4");
-      let cardBox = document.createElement("div").classList.add("card-box");
-      let musicCard = document.createElement("div").classList.add("music-card");
-      const musicPicSmall = document
-        .createElement("img")
-        .classList.add("music-pic-small");
-      let musicText = document.createElement("div").classList.add("music-text");
-      let title = document
-        .createElement("p")
-        .classList.add("music-title", "font-chubby");
-      let icon = document.createElement("img").classList.add("play-icon");
-      musicPicSmall.src =
-        "https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg";
-
-      musicCard.appendChild("musicPicSmall");
-      musicText.appendChild("title");
-      musicText.appendChild("icon");
-      musicCard.appendChild("musicText");
-      cardBox.appendChild("musicCard");
-      column.appendChild("cardBox");
-      firstRow.appendChild("column");
-      const response = await fetch(
-        "https://spotify-fetch.herokuapp.com/https://api.deezer.com/album/" + id
-      );
+      const response = await fetch(albumURL + id);
       const data = await response.json();
       console.log(data);
+
+      let firstRow = document.getElementById("good-time-row");
+      firstRow.innerHTML += `<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="card-box">
+          <div class="music-card">
+            <img class="music-pic-small" onmouseover="changeBackgroundColor()" onmouseout="removeGradient()" src="${data.cover}" height="80" width="80" alt="">
+            <div class="music-text">
+              <p class="music-title font-chubby">${data.title}</p>
+              <img class="play-icon" src="/imgs/play.svg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>`;
 
       return data;
     }
   });
 };
+
+window.onload = showData();
